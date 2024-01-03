e4375de64d766d3ad1bd7c6ae2b0b99e136377d5
Ricardo <rvw_po@mac.com>
2024-01-03 15:27:01 +0100

    clean up and improved docs for test.types.ts

--- test/Test.types.ts
+++ test/Test.types.ts
@@ -3,9 +3,10 @@ type TO_BE = 'ToBe'
 type TO_NOT_BE = 'ToNotBet'
 type TO_EQUAL = 'ToEqual'
 
-// Base Test
+// Base Test Case
 export type TestType<Result extends true> = Result
 
+// Polymorphic Assertion Handler
 export type Expect<Input, Output extends [string, unknown]> = Output[0] extends TO_EQUAL
   ? AssertEqual<Input, Output[1]>
   : Output[0] extends TO_BE
@@ -14,14 +15,14 @@ export type Expect<Input, Output extends [string, unknown]> = Output[0] extends 
       ? AssertNotBe<Input, Output[1]>
       : false
 
-// Assert input to be type
+// Declarative Wrappers
 export type ToBe<Output> = [TO_BE, Output]
-export type AssertToBe<Input, Type> = Input extends Type ? true : false
-
-// Assert input not to be type
 export type ToNotBe<Output> = [TO_NOT_BE, Output]
-export type AssertNotBe<Input, Type> = Input extends Type ? false : true
-
-// Assert return type of input to equal output
 export type ToEqual<Output> = [TO_EQUAL, Output]
+
+// Assert input to be type
+export type AssertToBe<Input, Type> = Input extends Type ? true : false
+// Assert input not to be type
+export type AssertNotBe<Input, Type> = Input extends Type ? false : true
+// Assert return type of input to equal output
 export type AssertEqual<Input, Output> = (<T>() => T extends Input ? 1 : 2) extends <T>() => T extends Output ? 1 : 2 ? true : false
